5bc1f06f29086c39f8853c9ef8cb04a40898a304
Shraddha13Jain <shraddhaj829@gmail.com>
2021-01-23 17:15:19 +0530

    displayed card

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,15 +1,20 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import axios from "axios";
 import Card from "react-bootstrap/Card";
 
 function Blogs (){
     
+    const [blogs , setBlogs] = useState([]);
+
     useEffect(() => {
       const dataFetch=async() => {
           try{
-           const res = await axios.get("https://blogapp1302.herokuapp.com/api/blogs");
-           console.log(res);
+           const res = await axios.get("https://mostlypandatuts.herokuapp.com/api/blogs");
+           console.log(res.data);
+           if(res.data){
+               setBlogs(res.data);
+           }
           }
           catch(error){
               console.log(error);
@@ -21,21 +26,36 @@ function Blogs (){
     }, [])
 
     return (
-       <>
-       <Card style={{ width: '18rem' }}>
-         <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-            </Card.Text>
-         </Card.Body>  
-     </Card>
+    <>
+         {(blogs) ? (
+             <>
+                {blogs.map( (blog) => (
+                        <Card style={{ width: '18rem' }}>
+                        <Card.Body>
+                           <Card.Title>{blog.title}</Card.Title>
+                           <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                           <Card.Text>
+                             Some quick example text to build on the card title and make up the bulk of
+                             the card's content.
+                           </Card.Text>
+                        </Card.Body>  
+                    </Card>
+                ))
+                
+                }
+             </>
+         ) : (
+             <>
+             
+             </>
+         )
+         }
+
+      
        
        
        
-       </>
+    </>
    )
 
 } 
